de5cbaec3ea3e888aa6048ba123c72f13d23a422
ConsensusDebateBot <44487499+ConsensusDebateBot@users.noreply.github.com>
2018-11-07 10:30:13 -0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,8 +1,6 @@
 import time
 from collections import deque
 
-from thread import Thread
-
 from praw.models import Comment
 
 
@@ -11,8 +9,6 @@ class PushshiftStream:
 	def __init__(self, reddit, subreddit_name, last_seen=None):
 		self.reddit = reddit
 		self.subreddit_name = subreddit_name
-		self._ids = []
-		self.link_id = []
 		self._url = 'https://api.pushshift.io/reddit/comment/search/'
 		self._last_req_time = 0
 		self._last_seen = last_seen or deque(maxlen=100)
@@ -22,8 +18,6 @@ class PushshiftStream:
 		else:
 			self._after = int(time.time())
 		self._params = {'subreddit': self.subreddit_name,
-		                'ids'      : self._ids,
-		                'link_id'  : self.link_id,
 						'size'     : self._size,
 						'after'    : self._after}
 
@@ -32,26 +26,9 @@ class PushshiftStream:
 			diff = time.time() - self._last_req_time
 			if diff < 1:
 				time.sleep(1 - diff)
-			try:
-			    req = self.reddit.get(self._url, params=self._params)
-			except Exception:
-			    pass
+			req = self.reddit.get(self._url, params=self._params)
 			self._last_req_time = time.time()
-			for mention in self.reddit.inbox.mentions():
-			    botrepliedtothread = 0
-			    if mention.subreddit != self.subreddit_name:
-			        tempmentionreq = self.reddit.get(self._url, params= {'link_id' : mention.submission})
-			        tempmentiondata = tempmentionreq['data']
-			        for tempreqcomment in tempmentiondata:
-			            if tempreqcomment['author'] == 'ConsensusDebateBot':
-			                botrepliedtothread += 1
-			        if botrepliedtothread > 0:
-			            continue
-			        else: self.link_id.append(mention.submission)
-			try:
-			    pushshift_comments = req['data']
-			except Exception:
-			    continue
+			pushshift_comments = req['data']
 			new_praw_comments = []
 			for pushshift_comment in pushshift_comments:
 				comment = Comment(self.reddit, _data=pushshift_comment)

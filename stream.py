54072288838ed49b96724d64ca0776e9922cb231
ConsensusDebateBot <44487499+ConsensusDebateBot@users.noreply.github.com>
2018-11-24 10:17:35 -0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,19 +1,21 @@
 import time
 from collections import deque
 
-from thread import Thread
+from data import Data
 
 from praw.models import Comment
 
 
 class PushshiftStream:
 
-	def __init__(self, reddit, subreddit_name, last_seen=None):
+	def __init__(self, reddit, subreddit_name, last_seen=None, **options):
 		self.reddit = reddit
 		self.subreddit_name = subreddit_name
-		self._ids = []
-		self.link_id = []
+		self.data = Data(save_rate=options.get('save_data_every', 10))
+		self.data.load()
 		self._url = 'https://api.pushshift.io/reddit/comment/search/'
+		self._link_id_url = 'https://api.pushshift.io/reddit/search/submission/'
+		self._comment_ids_url = 'https://api.pushshift.io/reddit/submission/comment_ids/'
 		self._last_req_time = 0
 		self._last_seen = last_seen or deque(maxlen=100)
 		self._size = 500
@@ -21,9 +23,7 @@ class PushshiftStream:
 			self._after = int(self._last_seen[-1].created_utc) - 1
 		else:
 			self._after = int(time.time())
-		self._params = {'subreddit': self.subreddit_name,
-		                'ids'      : self._ids,
-		                'link_id'  : self.link_id,
+		self._params = {#'subreddit': self.subreddit_name,
 						'size'     : self._size,
 						'after'    : self._after}
 
@@ -32,26 +32,37 @@ class PushshiftStream:
 			diff = time.time() - self._last_req_time
 			if diff < 1:
 				time.sleep(1 - diff)
-			try:
-			    req = self.reddit.get(self._url, params=self._params)
-			except Exception:
-			    pass
+			twoweekstime = 1209600
+			twoweeksago = time.time() - twoweekstime
+			mention_comments = []
+			req = self.reddit.get(self._url, params=self._params)
 			self._last_req_time = time.time()
-			for mention in self.reddit.inbox.mentions():
-			    botrepliedtothread = 0
-			    if mention.subreddit != self.subreddit_name:
-			        tempmentionreq = self.reddit.get(self._url, params= {'link_id' : mention.submission})
-			        tempmentiondata = tempmentionreq['data']
-			        for tempreqcomment in tempmentiondata:
-			            if tempreqcomment['author'] == 'ConsensusDebateBot':
-			                botrepliedtothread += 1
-			        if botrepliedtothread > 0:
-			            continue
-			        else: self.link_id.append(mention.submission)
-			try:
-			    pushshift_comments = req['data']
-			except Exception:
-			    continue
+			def bot_stream_comments(self, thread):
+			    try:
+			        botmentionreq = self.reddit.get(self._url, params= {'author' : 'ConsensusDebateBot',
+			                                                        'after'  : twoweeksago})
+			        botmentiondata = botmentionreq['data']
+			        for botreqcomment in botmentiondata:
+				        if botreqcomment.subreddit != self.subreddit:
+				            thread.sticky_comment = botreqcomment
+			    except Exception:
+				    pass
+			mentionpostreq = self.reddit.get(self._url, params= {'q' : 'u/ConsensusDebateBot', 'after' : 1541786417})
+			mentionpostdata = mentionpostreq['data']
+			for mentionpost in mentionpostdata:
+			    mentioncomment = Comment(self.reddit, _data=mentionpost)
+			    print(mentioncomment.submission)
+			    mentionpostcommentids = self.reddit.get(self._comment_ids_url, params= {'link_id' : mentioncomment.submission})
+			    mentionpostcomments = self.reddit.get(self._url, params= {'ids' : mentionpostcommentids})
+			    mention_comments.extend(mentionpostcomments)
+			    print(mentionpostcomments)
+			pushshift_comments = req['data']
+			pushshift_comments.extend(mention_comments)
+			final_pushshift_comments = []
+			for pushshift_comment in pushshift_comments:
+			    if pushshift_comment not in final_pushshift_comments:
+			        final_pushshift_comments.append(pushshift_comment)
+			pushshift_comments = final_pushshift_comments
 			new_praw_comments = []
 			for pushshift_comment in pushshift_comments:
 				comment = Comment(self.reddit, _data=pushshift_comment)
